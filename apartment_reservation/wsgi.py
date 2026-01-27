5fc679b5b6be8a9c0da87f02b15bb7e4c0a36596
ramodjarod <ramosjarod04@gmail.com>
2026-01-27 17:55:25 +0800

    Fix Vercel deployment: update vercel.json and wsgi entry point

--- apartment_reservation/wsgi.py
+++ apartment_reservation/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apartment_reservation.settings')
 
 application = get_wsgi_application()
+
+app = application
