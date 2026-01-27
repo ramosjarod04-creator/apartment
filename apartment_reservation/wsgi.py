19e54d9a7b3b6a389f61be033371efa2ce320217
ramodjarod <ramosjarod04@gmail.com>
2026-01-27 19:49:30 +0800

    Create cloud superuser

--- apartment_reservation/wsgi.py
+++ apartment_reservation/wsgi.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 from django.core.management import call_command
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apartment_reservation.settings')
 
@@ -18,8 +20,8 @@ app = application
 
 # This is the "Magic Fix": It runs migrations whenever the app starts
 try:
-    print("Running migrations...")
-    call_command('migrate', interactive=False)
-    print("Migrations completed successfully!")
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'YourStrongPassword123')
+        print("Superuser created successfully!")
 except Exception as e:
-    print(f"Migration failed: {e}")
+    print(f"Superuser creation failed: {e}")
