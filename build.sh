83ca5c20276bfe80064a671ed89fc1c978bf922c
ramodjarod <ramosjarod04@gmail.com>
2026-01-27 19:38:08 +0800

    Force build settings update

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Run migrations - THIS IS WHAT FIXES YOUR ERROR
 python3.12 manage.py migrate --noinput
-
-# Collect static files
 python3.12 manage.py collectstatic --noinput
