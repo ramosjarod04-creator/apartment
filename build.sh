b7b3b92f49b9bc8bd26e38000d268bf41d5d12e4
ramodjarod <ramosjarod04@gmail.com>
2026-01-27 19:26:14 +0800

    Add build script and run migrations

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Install dependencies
+# 1. Install dependencies
 pip install -r requirements.txt
 
-# Run migrations
-python3.12 manage.py migrate --noinput
+# 2. Run Database Migrations (This fixes your error!)
+python manage.py migrate --noinput
 
-# Collect static files (if you use WhiteNoise)
-python3.12 manage.py collectstatic --noinput
+# 3. Collect Static Files
+python manage.py collectstatic --noinput
